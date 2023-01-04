5683682508d3835dc5300bf99d2eca431e65d628
shehan <10749822@students.plymouth.ac.uk>
2023-01-04 21:08:22 +0530

    Add project files.

---
+++ WebApplicationApi6/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationApi6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Users()
+        {
+            var users= new string[] { "user1", "user2" };
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult NewUser()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser()
+        {
+            var wrong = true;
+            if (wrong)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
